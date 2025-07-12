40aa7e39b3666c0e347ec35bf66d36fcd05f90ef
realsoumyadey07 <soumyadipdey802@gmail.com>
2025-07-12 20:52:25 +0530

    login yet to be done

--- app/src/main/java/org/example/entities/Ticket.java
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -6,6 +6,7 @@ public class Ticket {
     private String ticketId;
     private String userId;
     private String source;
+    private String destination;
     private Date dateOfTravel;
     private Train train;
 }
